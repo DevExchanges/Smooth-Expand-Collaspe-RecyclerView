d1b224f5d916f6a2ba0c4975c47707d6525c1dbd
DevExchanges <wandered.vip.007@gmail.com>
2016-08-05 16:59:28 +0700

    push

---
+++ app/src/main/java/info/devexchanges/smoothexpandcollapserecyclerview/MainActivity.java
@@ -0,0 +1,25 @@
+package info.devexchanges.smoothexpandcollapserecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        SummonerAdapter summonerAdapter = new SummonerAdapter(this);
+        recyclerView.setAdapter(summonerAdapter);
+    }
+}
